fac38016a8fc036ddf20e63f5c04953d882c9241
Rafael Soares <107703258+rsoaresdev@users.noreply.github.com>
2024-06-19 22:38:12 +0100

    feat: Implement summary API

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,7 @@
 import { twMerge } from "tailwind-merge";
 import { type ClassValue, clsx } from "clsx";
 import { toast } from "sonner";
+import { eachDayOfInterval, isSameDay } from "date-fns";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -31,4 +32,44 @@ export function toastAlert(message: string, type: "success" | "danger") {
   } else {
     toast.error(message, { richColors: true });
   }
+}
+
+export function calculatePercentageChange(current: number, previous: number) {
+  if (previous === 0) {
+    return previous === current ? 0 : 100;
+  }
+
+  return ((current - previous) / previous) * 1000;
+}
+
+export function fillMissingDays(
+  activeDays: {
+    date: Date;
+    income: number;
+    expenses: number;
+  }[],
+  startDate: Date,
+  endDate: Date
+) {
+  if (activeDays.length === 0) {
+    return [];
+  }
+
+  const allDays = eachDayOfInterval({ start: startDate, end: endDate });
+
+  const transactionsByDay = allDays.map((day) => {
+    const found = activeDays.find((d) => isSameDay(d.date, day));
+
+    if (found) {
+      return found;
+    } else {
+      return {
+        date: day,
+        income: 0,
+        expenses: 0,
+      };
+    }
+  });
+
+  return transactionsByDay;
 }
