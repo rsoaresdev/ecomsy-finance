938d2441b6be7245489fc416faf2f51b441c32a6
Rafael Soares <107703258+rsoaresdev@users.noreply.github.com>
2024-06-20 02:06:44 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,7 +9,10 @@ const nextConfig = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://ecomsy.site" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://www.ecomsy.site",
+          },
           {
             key: "Access-Control-Allow-Methods",
             value: "GET, DELETE, PATCH, POST, PUT",
